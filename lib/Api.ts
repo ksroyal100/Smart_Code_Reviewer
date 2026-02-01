0f5eaea0e87456b031579022635bcdfc388fc8d8
kishan <guptakishan492@gmail.com>
2026-02-01 18:13:16 +0530

    changing way of using api endpoint

--- lib/Api.ts
+++ lib/Api.ts
@@ -1,3 +1,5 @@
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ||
-  "http://localhost:8080";
+const localhost = "http://localhost:8080";
+const server = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export const API_BASE_URL =server;
+
